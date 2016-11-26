e9ed1f42640d2ffbd99f8a8db605153dc939e8d8
mark-toma <mark@mark-toma.com>
2016-11-25 20:48:48 -0500

    Fixes bug
    Destroys timer in stopStreaming before deleting myoData so that the
    timer callback is deleted before it attemps to access myoData

--- MyoMex/MyoMex.m
+++ MyoMex/MyoMex.m
@@ -127,10 +127,11 @@ classdef MyoMex < handle
     
     function delete(this)
       % delete  Clean up MyoMex instance of MEX function myo_mex
+      this.stopStreaming();
       for ii = 1:length(this.myoData)
         delete(this.myoData(ii));
       end
-      this.stopStreaming();
+      
       
       [fail,emsg] = MyoMex.myo_mex_delete;
       assert(~fail,...
